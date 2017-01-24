d50f8da3ed8e86d78be170560fb0e77574fc00ce
Villa, Camilo (EX1 - Philadelphia) <camilo.villa@yuxipacific.com>
2017-01-23 23:41:45 -0500

    Updating to implement a singleton class example

---
+++ IMS.ModelingLib/IMS.StupidParadigm/Preuba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.ModelingLib.IMS.StupidParadigm
+{
+    public class Proof
+    {
+        private static void Main(string[] args)
+        {
+            SessionData obj1 = SessionData.getInstance;
+            SessionData obj2 = SessionData.getInstance;
+
+            Console.WriteLine("obj1 & obj2 are {0}", obj1 == obj2 ? "equals" : "differents");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IMS.ModelingLib/IMS.StupidParadigm/SessionData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web;
+
+namespace IMS.ModelingLib.IMS.StupidParadigm
+{
+
+    public class SessionData
+    {
+        private static readonly Lazy<SessionData> instance = new Lazy<SessionData>(() => new SessionData());
+
+        private string _contactName;
+        private int _contactId;
+        private string _currentTab;
+
+        public string ContactName
+        {
+            get { _contactName = (string)HttpContext.Current.Session["_contactName_"]; return _contactName; }
+            set { HttpContext.Current.Session["_contactName_"] = value; }
+        }
+
+        public int ContactId
+        {
+            get { _contactId = (int)HttpContext.Current.Session["_contactId_"]; return _contactId; }
+            set { HttpContext.Current.Session["_contactId_"] = value; }
+        }
+
+        public string CurrentTab
+        {
+            get { _currentTab = (string)HttpContext.Current.Session["_currentTab_"]; return _currentTab; }
+            set { HttpContext.Current.Session["_currentTab_"] = value; }
+        }
+
+        public static SessionData getInstance
+        {
+            get { return instance.Value; }
+        }
+    }
+}
